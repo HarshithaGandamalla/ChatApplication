545ea3c30ec578ebf2400eaf10feb7412badd0a2
Harshitha Gandamalla <harsh1thagandama@ufl.edu>
2016-12-30 20:51:39 -0600

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,23 @@
-var socket= io();
+// var socket= io();
 
-socket.on('Connect',function(){
+// socket.on('Connect',function(){
 	
-	console.log('Connected to socket');
-}); 
+// 	console.log('Connected to socket');
+// }); 
+
+// socket.emit('message',function (message){
+	
+// 	console.log('Message sent');
+// 	console.log(message.text);
+// } );
+
+var socket = io();
+
+socket.on('connect', function () {
+	console.log('Conncted to socket.io server!');
+});
+
+socket.on('message', function (message) {
+	console.log('New message:');
+	console.log(message.text);
+});
